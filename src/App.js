9352ce8835868ac0bbc6d8231f268a49190353ee
samin-sharar-val <samin@valerianfunds.com>
2022-08-24 00:09:56 +0600

    SSL check

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className='App'>SSL Check</div>;
+  return (
+    <>
+      {" "}
+      <div className='App'>SSL Check</div>;
+      <button
+        class='your-button-class'
+        id='sslczPayBtn'
+        token='if you have any token validation'
+        postdata='10000000'
+        order='If you already have the transaction generated for current order'
+        endpoint='http://localhost:8000/init'
+      >
+        {" "}
+        Pay Now
+      </button>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+(function (window, document) {
+  var loader = function () {
+    var script = document.createElement("script"),
+      tag = document.getElementsByTagName("script")[0];
+    script.src =
+      "https://sandbox.sslcommerz.com/embed.min.js?" +
+      Math.random().toString(36).substring(7);
+    tag.parentNode.insertBefore(script, tag);
+  };
 
+  window.addEventListener
+    ? window.addEventListener("load", loader, false)
+    : window.attachEvent("onload", loader);
+})(window, document);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
