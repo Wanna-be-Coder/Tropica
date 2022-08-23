5b2818a3df5a54eabecd0d3b1b835bcae36b3e6e
samin-sharar-val <samin@valerianfunds.com>
2022-08-24 00:36:01 +0600

    SSL demo run done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const handleSubmit = () => {
+    const data = {
+      name: "Yo",
+      total_amount: 10000,
+    };
+
+    axios
+      .get("http://localhost:8000/init", data)
+      .then((resp) => {
+        // console.log(resp);
+        window.location.replace(resp.data.gatewayPageUrl);
+        // window.location.replace(resp.gatewayPageUrl);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+
   return (
     <>
       {" "}
@@ -8,10 +27,11 @@ function App() {
       <button
         class='your-button-class'
         id='sslczPayBtn'
-        token='if you have any token validation'
-        postdata='10000000'
-        order='If you already have the transaction generated for current order'
-        endpoint='http://localhost:8000/init'
+        // token='if you have any token validation'
+        // postdata='10000000'
+        // order='If you already have the transaction generated for current order'
+        // endpoint='http://localhost:8000/init'
+        onClick={() => handleSubmit()}
       >
         {" "}
         Pay Now
